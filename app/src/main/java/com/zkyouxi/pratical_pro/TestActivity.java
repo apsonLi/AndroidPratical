ba2e1017eff2abb24949a01021bdf65457123270
licuisong <licuisong@izkun.com>
2022-02-21 16:00:38 +0800

    [feat]:sdk hook 资源合并教学

--- app/src/main/java/com/zkyouxi/pratical_pro/TestActivity.java
+++ app/src/main/java/com/zkyouxi/pratical_pro/TestActivity.java
@@ -3,6 +3,7 @@ package com.zkyouxi.pratical_pro;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -15,18 +16,29 @@ import java.lang.reflect.Method;
 public class TestActivity extends Activity implements View.OnClickListener {
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_sdktesst);
+//        Button button = findViewById(R.id.sdk_test_btn_login);
+        Button button = findViewById(getResources().getIdentifier("sdk_test_btn_login", "id", getPackageName()));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(TestActivity.this, "onclick sucess ", Toast.LENGTH_SHORT).show();
+            }
+        });//  nullPointExev
     }
 
 
     @Override
     public void onClick(View view) {
 
+    }
+
+    public static void main(String... args) {
+
+        System.out.println(args.length);
+        System.out.println(args[0]);
     }
 }
